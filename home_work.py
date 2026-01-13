60b8ec33045809cd842577e07c295d46798d407a
ZaritskayaL <zaritskaya.l@seznam.cz>
2026-01-13 12:55:07 +0100

    Commit message for changes made to home_work.py

--- home_work.py
+++ home_work.py
@@ -1,1 +1,17 @@
+language = {"name": "Python", "version": 3.11}
+print(language)
+language["version"] = 3.12
+language["year"] =1991
+print(language)
+del language["year"]
+print(language)
+
 # get_days_from_today(date)
+from datetime import datetime, timedelta
+def get_days_from_today(date_str):
+    try:
+        start_date = datetime.strptime(date_str, 'yyyy-MM-dd')
+        return(datetime.today() - start_date)
+    except ValueError:
+        return None
+print(get_days_from_today('2021-10-09'))
